88a022e512419294c9334b08761f69e731d2d507
Leandro Menezes <lelemm@gmail.com>
2025-09-11 10:22:56 -0300

    Calendar language fix

--- packages/loot-core/src/shared/locale.ts
+++ packages/loot-core/src/shared/locale.ts
@@ -1,22 +1,32 @@
-import * as locales from 'date-fns/locale';
+// import * as locales from 'date-fns/locale';
+
+// export function getLocale(language: string) {
+//   if (!language || typeof language !== 'string') {
+//     return locales.enUS;
+//   }
+
+//   let localeKey = language.replace('-', '') as keyof typeof locales;
+
+//   if (localeKey in locales) {
+//     return locales[localeKey];
+//   }
+
+//   //if language was not found with four letters, try with two
+//   localeKey = language.replace('-', '').substring(0, 2) as keyof typeof locales;
+
+//   if (localeKey in locales) {
+//     return locales[localeKey];
+//   }
+
+//   return locales.enUS;
+// }
+
+import { ptBR } from 'date-fns/locale';
 
 export function getLocale(language: string) {
-  if (!language || typeof language !== 'string') {
-    return locales.enUS;
+  if (language === 'pt-BR') {
+    return ptBR;
   }
 
-  let localeKey = language.replace('-', '') as keyof typeof locales;
-
-  if (localeKey in locales) {
-    return locales[localeKey];
-  }
-
-  //if language was not found with four letters, try with two
-  localeKey = language.replace('-', '').substring(0, 2) as keyof typeof locales;
-
-  if (localeKey in locales) {
-    return locales[localeKey];
-  }
-
-  return locales.enUS;
+  return ptBR;
 }
